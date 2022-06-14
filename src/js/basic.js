ee19f8fa601003e9b5b4f21bb05ffa1396e3e1ca
colins1 <maxrozhkov1@gmail.com>
2022-06-15 02:24:43 +0300

    add

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,82 +1,64 @@
-export class Characte {
-  constructor (name, type, attack, defence) {
-    this.name = name;
-    this.type = type;
+const params = {
+  Bowman: {
+    attack: 25,
+    defense: 25,
+  },
+  Swordsman: {
+    attack: 40,
+    defense: 10,
+  },
+  Magician: {
+    attack: 10,
+    defense: 40,
+  },
+  Undead: {
+    attack: 25,
+    defense: 25,
+  },
+  Zombie: {
+    attack: 40,
+    defense: 10,
+  },
+  Daemon: {
+    attack: 10,
+    defense: 40,
+  }
+};
+
+
+export default class Character {
+  constructor (name, type) {
+    this.name = this.nameEdit(name);
+    this.type = this.typeEdit(type);
+  }
+
+  setStats() {
     this.health = 100;
     this.level = 1;
-    this.attack = attack;
-    this.defence = defence;
+    this.attack = params[this.type].attack;
+    this.defence = params[this.type].defense;
   }
+
+  
+  nameEdit(name) {
+    if (typeof name !== 'string') {
+      throw new Error('please edit to string');
+    }
+    if (name.length < 1) {
+     throw new Error('min 3 string');
+    }
+    if (name.length > 15) {
+      throw new Error('max 10 string');
+    }
+    return name;
+  };
+
+  typeEdit(type) {
+    if (type in params) {
+      return type;
+    }
+    throw new Error('you selected a non-existent type');
+  };
 }
 
-export class Bowerman extends Characte {
-  constructor (name, type, attack, defence) {
-    super(name);
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = 25;
-    this.defence = 25;
-  }
-}
 
-export class Swordsman extends Characte {
-  constructor (name, type, attack, defence) {
-    super(name);
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = 40;
-    this.defence = 10;
-  }
-}
-
-export class Magician extends Characte {
-  constructor (name, type, attack, defence) {
-    super(name);
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = 10;
-    this.defence = 40;
-  }
-}
-
-export class Daemon extends Characte {
-  constructor (name, type, attack, defence) {
-    super(name);
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = 10;
-    this.defence = 40;
-  }
-}
-
-export class Undead extends Characte {
-  constructor (name, type, attack, defence) {
-    super(name);
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = 25;
-    this.defence = 25;
-  }
-}
-
-export class Zombie extends Characte {
-  constructor (name, type, attack, defence) {
-    super(name);
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = 40;
-    this.defence = 10;
-  }
-}
